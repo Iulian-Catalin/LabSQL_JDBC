53bcbcbc7b27dd22ddd802714b55f5aea88c3511
Iulian-Catalin <constantin.94.catalin@gmail.com>
2023-04-08 02:36:19 +0300

    v1.02

--- src/main/java/DemoCRUDPeUsers.java
+++ src/main/java/DemoCRUDPeUsers.java
@@ -13,7 +13,7 @@ public class DemoCRUDPeUsers {
         List result = obiect.readAllUsers();
       System.out.println(result);
 
-//        User u = new User("gigica", "password1");
+//        User u = new User("gigica2", "password1");
 //
 //        boolean isAdded = obiect.insert(u);
 //        System.out.println(isAdded);
